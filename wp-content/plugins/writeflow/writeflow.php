9fc447aef0fb9e625d1e75fd178f1792c1e98587
ardyerd <75718934+ardyerd@users.noreply.github.com>
2026-02-11 11:38:52 +0700

    [feature] - initial commit writeflow plugin

---
+++ wp-content/plugins/writeflow/writeflow.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Plugin Name: WriteFlow
+ * Description: A plugin to enhance the writing experience in WordPress.
+ * Version: 1.0
+ * Author: Ardy - MadeIndonesia
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly.
+}
+
+function writeflow_register_meta() {
+    register_post_meta( 'post', '_writeflow_outline', [
+        'type' => 'string',
+        'single' => true,
+        'show_in_rest' => true,
+    ] );
+
+    register_post_meta('post', '_writeflow_idea', [
+        'type' => 'string',
+        'single' => true,
+        'show_in_rest' => true,
+    ]);
+}
+add_action( 'init', 'writeflow_register_meta' );
+
+function writeflow_enqueue_assets() {
+    wp_enqueue_script( 
+        'writeflow-editor', 
+        plugins_url('build/index.js', __FILE__), 
+        ['wp-plugins', 'wp-edit-post', 'wp-element', 'wp-components', 'wp-data'],
+        filemtime( plugin_dir_path( __FILE__ ) . 'build/index.js' ) 
+    );
+}
+add_action( 'enqueue_block_editor_assets', 'writeflow_enqueue_assets' );
